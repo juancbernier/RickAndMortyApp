33b220bbfc69e8fad10c24f9f54feb4a190fb1ac
Juan Bernier <juanberniercr@unimagdalena.edu.co>
2023-04-20 08:38:12 -0500

    Initial Commit

---
+++ RickAndMortyApp/Data/Remote DataSource/APIEndPoints.swift
@@ -0,0 +1,16 @@
+//
+//  APIEndPoints.swift
+//  RickAndMortyApp
+//
+//  Created by Juan Bernier on 20/04/23.
+//
+
+import Foundation
+
+
+struct APIEndPoint{
+
+    
+    
+    
+}
---
+++ RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Juan Bernier on 20/04/23.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
